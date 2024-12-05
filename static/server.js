b761dd6b45de5c97af846a05c13add7aa26e2f02
AmitGuha04 <guha.amit@outlook.com>
2024-12-05 11:55:59 -0800

    Added some code to the JS side
    Cleaned up a bit and added skeleton code for appending images to container.

--- static/server.js
+++ static/server.js
@@ -9,37 +9,54 @@ function setBoard(){
             const img = document.createElement('img');
             img.src = 'light.png';
             //Append to container when created
-            
+            const postPiece = document.createElement('div');
+            postPiece.classList.add()//Container name here
 
             //On click
-            onclock = _ => { console.log(i) }
+            onclick = _ => {
+                console.log(i);
+                //Call a function to check if other square is clicked
+                //If so move piece to that square
+            }
             
         } else if(temppiece == 'b'){
             const img = document.createElement('img');
             img.src = 'dark.png';
             //Append to container when created
+            postPiece.classList.add()//Container name here
             
-
             //On click
-            onclock = _ => { console.log(i) }
+            onclick = _ => {
+                console.log(i);
+                //Call a function to check if other square is clicked
+                //If so move piece to that square
+            }
 
         } else if(temppiece == 'W'){
             const img = document.createElement('img');
             img.src = 'dlight.png';
             //Append to the container when created
-            
+            postPiece.classList.add()//Container name here
 
             //On click
-            onclock = _ => { console.log(i) }
+            onclick = _ => {
+                console.log(i);
+                //Call a function to check if other square is clicked
+                //If so move piece to that square
+            }
 
         } else if(temppiece == 'B'){
             const img = document.createElement('img');
             img.src = 'ddark.png';
             //Append to the container
-            
+            postPiece.classList.add()//Container name here
 
             //On Click
-            onclock = _ => { console.log(i) }
+            onclick = _ => {
+                console.log(i);
+                //Call a function to check if other square is clicked
+                //If so move piece to that square
+            }
 
         } else if(temppiece == '0'){
             const img = document.createElement('img');
